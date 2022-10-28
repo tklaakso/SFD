d9b80c3bac3893827856cc2e97c15f557fe484fb
Troy Laakso <laaksotroy@gmail.com>
2022-10-27 21:17:29 -0400

    Initial commit

---
+++ server/core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    path('', views.front, name='front'),
+    path('', include(router.urls)),
+]
